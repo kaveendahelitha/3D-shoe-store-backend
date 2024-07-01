8e293c05e862d737a0455cbd8e6bb55057e44173
Bodhika-Git <105534658+Bodhika-Git@users.noreply.github.com>
2024-07-01 10:19:49 -0700

    Add relevant backend codes for sales

--- src/main/java/com/shoe/shoemanagement/entity/Sale.java
+++ src/main/java/com/shoe/shoemanagement/entity/Sale.java
@@ -1,24 +1,42 @@
 package com.shoe.shoemanagement.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "sales")
 public class Sale {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "item_name")
+    private String itemName;
+
+    @Column(name = "given_price")
+    private double givenPrice;
+
+    @Column(name = "sold_price")
+    private double soldPrice;
+
+    @Column(name = "profit")
+    private double profit;
+
+    @Column(name = "date")
     private LocalDate date;
-    private int uv;
-    private int pv;
-    private int amt;
+    public Sale() {
+    }
+
+    public Sale(String itemName, double givenPrice, double soldPrice) {
+        this.itemName = itemName;
+        this.givenPrice = givenPrice;
+        this.soldPrice = soldPrice;
+        this.profit = soldPrice - givenPrice;
+    }
 
     // Getters and Setters
-
     public Long getId() {
         return id;
     }
@@ -27,35 +45,46 @@ public class Sale {
         this.id = id;
     }
 
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public double getGivenPrice() {
+        return givenPrice;
+    }
+
+    public void setGivenPrice(double givenPrice) {
+        this.givenPrice = givenPrice;
+    }
+
+    public double getSoldPrice() {
+        return soldPrice;
+    }
+
+    public void setSoldPrice(double soldPrice) {
+        this.soldPrice = soldPrice;
+    }
+
+    public double getProfit() {
+        return profit;
+    }
+
+    public void setProfit(double profit) {
+        this.profit = profit;
+    }
+
+
+
     public LocalDate getDate() {
         return date;
     }
-
     public void setDate(LocalDate date) {
         this.date = date;
     }
-
-    public int getUv() {
-        return uv;
-    }
-
-    public void setUv(int uv) {
-        this.uv = uv;
-    }
-
-    public int getPv() {
-        return pv;
-    }
-
-    public void setPv(int pv) {
-        this.pv = pv;
-    }
-
-    public int getAmt() {
-        return amt;
-    }
-
-    public void setAmt(int amt) {
-        this.amt = amt;
-    }
 }
+
+
