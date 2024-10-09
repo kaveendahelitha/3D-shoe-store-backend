d1c361be79516f2be044142a2c7ba75ff2ba9622
Bodhika-Git <105534658+Bodhika-Git@users.noreply.github.com>
2024-10-09 10:34:15 -0700

    Adding changes for image uploading to the database

--- src/main/java/com/shoe/shoemanagement/controller/ImageUploadController.java
+++ src/main/java/com/shoe/shoemanagement/controller/ImageUploadController.java
@@ -1,6 +1,7 @@
 package com.shoe.shoemanagement.controller;
 
-
+import com.shoe.shoemanagement.entity.ImageModel;
+import com.shoe.shoemanagement.repository.ImageRepository;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -21,16 +22,30 @@ import java.nio.file.Paths;
 @RequestMapping("/api/upload")
 @CrossOrigin
 public class ImageUploadController {
+
     private static String imageDirectory = System.getProperty("user.dir") + "/images/";
+    private final ImageRepository imageRepository;
+
+    public ImageUploadController(ImageRepository imageRepository) {
+        this.imageRepository = imageRepository;
+    }
 
     @RequestMapping(value = "/image", produces = {MediaType.IMAGE_PNG_VALUE, "application/json"})
-    public ResponseEntity<?> uploadImage(@RequestParam("imageFile")MultipartFile file,
+    public ResponseEntity<?> uploadImage(@RequestParam("imageFile") MultipartFile file,
                                          @RequestParam("imageName") String name) {
         makeDirectoryIfNotExist(imageDirectory);
         Path fileNamePath = Paths.get(imageDirectory,
                 name.concat(".").concat(FilenameUtils.getExtension(file.getOriginalFilename())));
+
         try {
             Files.write(fileNamePath, file.getBytes());
+
+            // Create an ImageModel instance and save it to the database
+            ImageModel imageModel = new ImageModel();
+            imageModel.setImageName(name);
+            imageModel.setImageDescription("Image uploaded successfully");
+            imageRepository.save(imageModel); // Save to database
+
             return new ResponseEntity<>(name, HttpStatus.CREATED);
         } catch (IOException ex) {
             return new ResponseEntity<>("Image is not uploaded", HttpStatus.BAD_REQUEST);
