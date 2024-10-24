7804a86175a6ac28ef09b5c72ab51880a0cc1790
Bodhika-Git <105534658+Bodhika-Git@users.noreply.github.com>
2024-10-24 07:00:10 -0700

    dddd

--- src/main/java/com/shoe/shoemanagement/entity/DisableUser.java
+++ src/main/java/com/shoe/shoemanagement/entity/DisableUser.java
@@ -8,7 +8,7 @@ import jakarta.persistence.*;
 public class DisableUser {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(name = "left_checkbox")
